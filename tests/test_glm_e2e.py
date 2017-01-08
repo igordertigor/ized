1222645d5ad6b2851f909cdc5edbacad1df8d951
Ingo Fründ <mail@ingofruend.net>
2017-01-08 17:41:54 +0100

    notes for glm e2e tests

---
+++ tests/test_glm_e2e.py
@@ -0,0 +1,9 @@
+# Test that qr docomposition gives the same results on chunks and in full go
+# Test that more data converge to ground truth
+# Test that increasing ridge penalty increases training error and decreases parameter vector
+# Irrespective of basis, we should be able to approximate an arbitrary smooth function more accurately
+# constraints?
+# with multiple penalties, make sure that the gcv for the final penalty is better than a coarse grid search
+# test a normal glm
+# test a binomial logistic glm
+# test a poisson log glm
